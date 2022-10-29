611e76b4e5126fb3c0d876864bea940afd9f2cd8
Alexander Radyushin <a.radyushin@pm.me>
2022-10-28 22:15:48 -0300

    fix: update typescript definitions

--- Downloader.td.ts
+++ Downloader.td.ts
@@ -1,40 +1,37 @@
-import http from 'http';
+import http from "http";
 export default Downloader;
 
-
-interface DownloaderConfig{
-    url:string
-    directory?:string
-    fileName?:string
-    cloneFiles?:boolean
-    skipExistingFileName?:boolean
-    timeout?:number
-    maxAttempts?:number
-    headers?:object
-    httpsAgent?:any
-    proxy?:string
-    onError?(e:Error):void
-    onResponse?(r:http.IncomingMessage):boolean|void
-    onBeforeSave?(finalName:string):string|void
-    onProgress?(percentage:string,chunk:object,remaningSize:number):void
-    shouldStop?(e:Error):boolean|void
-    shouldBufferResponse?:boolean
-    useSynchronousMode?:boolean
-
+interface DownloaderConfig {
+  url: string;
+  directory?: string;
+  fileName?: string;
+  cloneFiles?: boolean;
+  skipExistingFileName?: boolean;
+  timeout?: number;
+  maxAttempts?: number;
+  delayBetweenAttempts?: number;
+  headers?: object;
+  httpsAgent?: any;
+  proxy?: string;
+  onAttempt?(attemptNumber: number): void | Promise<void>;
+  onError?(e: Error): void | Promise<void>;
+  onResponse?(r: http.IncomingMessage): boolean | void;
+  onBeforeSave?(finalName: string): string | void;
+  onProgress?(percentage: string, chunk: object, remaningSize: number): void;
+  shouldStop?(e: Error): boolean | void | Promise<void>;
+  shouldBufferResponse?: boolean;
+  useSynchronousMode?: boolean;
 }
 
-interface DownloaderReport{
-    downloadStatus:"COMPLETE"|"ABORTED"
-    filePath:string|null
+interface DownloaderReport {
+  downloadStatus: "COMPLETE" | "ABORTED";
+  filePath: string | null;
 }
-
 
 declare class Downloader {
+  constructor(config: DownloaderConfig);
 
-    constructor(config: DownloaderConfig);
+  download(): Promise<DownloaderReport>;
 
-    download():Promise<DownloaderReport>
-
-    cancel():void
-
+  cancel(): void;
 }
